aed95bab1fc381e1b9e8c2f19fbcfbd490e62ff0
sebastian <ziweidai@DESKTOP-8ETG8OI.localdomain>
2019-10-16 15:02:55 -0400

    mcrAgene in all 50 proteomes

--- mcrAgene_in_proteome_all/find_methanogens.sh
+++ mcrAgene_in_proteome_all/find_methanogens.sh
@@ -1,28 +1,25 @@
 #align mcrAgene
-#for i in {1..9}
-#do
-	#cat ./bioinformatics_project2019/proteomes/proteomes_0$i.fasta
-	#./muscle3.8.31_i86linux64 -in ./bioinformatics_project2019/ref_sequences/mcrAgene_0$i.fasta -out ./bioinformatics_project2019/ref_sequences/mcrAgene_0$i.afasta -maxiters 1
-#done
+for i in {1..9}
+do
+	cat ./bioinformatics_project2019/proteomes/proteomes_0$i.fasta
+	./muscle3.8.31_i86linux64 -in ./bioinformatics_project2019/ref_sequences/mcrAgene_0$i.fasta -out ./bioinformatics_project2019/ref_sequences/mcrAgene_0$i.afasta -maxiters 1
+done
 
-#for i in {10..18}
-#do
-	#./muscle3.8.31_i86linux64 -in ./bioinformatics_project2019/ref_sequences/mcrAgene_$i.fasta -out ./bioinformatics_project2019/ref_sequences/mcrAgene_$i.afasta -maxiters 1
-#done
-
+for i in {10..18}
+do
+	./muscle3.8.31_i86linux64 -in ./bioinformatics_project2019/ref_sequences/mcrAgene_$i.fasta -out ./bioinformatics_project2019/ref_sequences/mcrAgene_$i.afasta -maxiters 1
+done
 
 #building profile from alignment
-#for i in {1..9}
-#do	
-	#/afs/crc.nd.edu/user/z/zdai2/hmmer-3.2.1/src/hmmbuild /afs/crc.nd.edu/user/z/zdai2/bioinformatics_project2019/ref_sequences/mcrAgene_0$i.hmm /afs/crc.nd.edu/user/z/zdai2/bioinformatics_project2019/ref_sequences/mcrAgene_0$i.afasta
-#done
+for i in {1..9}
+do	
+	/afs/crc.nd.edu/user/z/zdai2/hmmer-3.2.1/src/hmmbuild /afs/crc.nd.edu/user/z/zdai2/bioinformatics_project2019/ref_sequences/mcrAgene_0$i.hmm /afs/crc.nd.edu/user/z/zdai2/bioinformatics_project2019/ref_sequences/mcrAgene_0$i.afasta
+done
 
-#for i in {10..18}
-#do
-	#/afs/crc.nd.edu/user/z/zdai2/hmmer-3.2.1/src/hmmbuild /afs/crc.nd.edu/user/z/zdai2/bioinformatics_project2019/ref_sequences/mcrAgene_$i.hmm /afs/crc.nd.edu/user/z/zdai2/bioinformatics_project2019/ref_sequences/mcrAgene_$i.afasta
-#done
-
-
+for i in {10..18}
+do
+	/afs/crc.nd.edu/user/z/zdai2/hmmer-3.2.1/src/hmmbuild /afs/crc.nd.edu/user/z/zdai2/bioinformatics_project2019/ref_sequences/mcrAgene_$i.hmm /afs/crc.nd.edu/user/z/zdai2/bioinformatics_project2019/ref_sequences/mcrAgene_$i.afasta
+done
 
 #search for mcrAgene sequence in proteomoes
 for i in {1..9} #mcrAgene numbering
